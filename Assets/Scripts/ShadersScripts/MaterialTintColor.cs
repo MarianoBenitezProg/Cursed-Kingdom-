c73edad72199f0080c787f69835333a7b1bdcacd
DuilioTommasiello <duilio.tommasiello@davinci.edu.ar>
2025-01-09 21:49:23 -0300

    shader para usar a fututro como heal y dmg
    shader tranqui y script que que cambia le color del sprite, para 3 colores, es funcional en el sprite normal, cuando tengamos la sheet lo voy a poder ir testeando mejor.

--- Assets/Scripts/ShadersScripts/MaterialTintColor.cs
+++ Assets/Scripts/ShadersScripts/MaterialTintColor.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class MaterialTintColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Material _Material;
+    private Color materialColor;
+    private float tintFadeSpeed;
+
+    private void Awake()
     {
-        
+        materialColor = new Color(1, 0, 0, 0);
+        setMaterial(transform.Find("Body").GetComponent<MeshRenderer>().material);
+        tintFadeSpeed = 6f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(materialColor.a > 0)
+        {
+            materialColor.a = Mathf.Clamp01(materialColor.a - tintFadeSpeed * Time.deltaTime);
+            _Material.SetColor("_Tint", materialColor);
+        }
+    }
+
+    public void setMaterial(Material material)
+    {
+        this._Material = material;
+    }
+
+    public void SetTintColor(Color color)
+    {
+        materialColor = color;
+        _Material.SetColor("_Tint", materialColor);
+    }
+    public void SetTintFadeSpeed(float tintFadeSpeed)
+    {
+        this.tintFadeSpeed = tintFadeSpeed;
     }
 }
