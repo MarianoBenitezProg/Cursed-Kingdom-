804e167602ee687989d5be8f1ac852cffc1c1b97
chispitasMB <107868815+chispitasMB@users.noreply.github.com>
2025-02-04 19:28:27 -0300

    inicio creacion slime
    falta agregar los comportamientos

--- Assets/SlimeFather.cs
+++ Assets/SlimeFather.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SlimeFather : MonoBehaviour
+public class SlimeFather : MonoBehaviour, ItakeDamage
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Slime Stats")]
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float damage = 20f;
+    [SerializeField] private int generationLevel = 0;  
+    [SerializeField] private float slimeScale = 1f;    
+
+    [Header("Split Settings")]
+    [SerializeField] private GameObject slimePrefab;   
+    [SerializeField] private float splitForce = 3f;   
+
+    [Header("Movement Settings")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpCooldown = 2f;
+    [SerializeField] private float detectionRange = 5f;
+
+
+    private float currentHealth;
+
+
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
+        transform.localScale = Vector3.one * slimeScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    private void Die()
+    {
+        if (generationLevel < 2) 
+        {
+            SpawnChildren();
+        }
+        Destroy(gameObject);
+    }
+
+    private void SpawnChildren()
+    {
+        Instantiate(slimePrefab);
+        Instantiate(slimePrefab);
+        Instantiate(slimePrefab);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        Debug.Log(currentHealth);
     }
 }
